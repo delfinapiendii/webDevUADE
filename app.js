526cf874638faabd4b4a70e97f36d3dd71e60dd4
Delfina Piendi <delfinapiendi@gmail.com>
2024-06-04 08:41:56 -0300

    1.0 JavaScript

---
+++ app.js
@@ -0,0 +1,14 @@
+
+/*
+let nombre = "Delfina";
+let apellido = "Piendibene";
+let edad = 22;*/
+let nombre = prompt("Ingresa tu nombre:");
+let apellido = prompt("Ingresa tu apellido:");
+let añoNacimiento = prompt("Ingresa tu año de nacimiento:");
+
+let diasVividos = (2024 - añoNacimiento) * 365
+
+alert(`Bienvenido ${nombre} ${apellido}, has vivido ${diasVividos} días aproximadamente.`);
+
+
