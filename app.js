c13e46ea72820227964344eb3e53b9969dfd6cbb
Delfina Piendi <delfinapiendi@gmail.com>
2024-06-06 18:20:28 -0300

    3.5 uva 3

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+
 /*
 let nombre = "Delfina";
 let apellido = "Piendibene";
@@ -11,20 +12,26 @@ let diasVividos = (2024 - añoNacimiento) * 365
 
 alert(`Bienvenido ${nombre} ${apellido}, has vivido ${diasVividos} días aproximadamente.`);
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('enviarBtn').addEventListener('click', function() {
+$(document).ready(function() {
+    $('#enviarBtn').on('click', function() {
         alert('Gracias por su consulta.');
     });
-});
-document.addEventListener('DOMContentLoaded', function() {
 
-    document.getElementById('ocultar-btn').addEventListener('click', function() {
+    $('#ocultar-btn').on('click', function() {
         $(".mi-imagen").hide();
         $("h1").text("¡No tenemos ninguna imagen!");
     });
 
-    document.getElementById('mostrar-btn').addEventListener('click', function() {
+    $('#mostrar-btn').on('click', function() {
         $(".mi-imagen").show();
         $("h1").text("¡Mirá la siguiente imagen!");
+    });
+
+    $('#modoClaro').on('click', function() {
+        $('body').removeClass('dark-mode');
+    });
+
+    $('#modoOscuro').on('click', function() {
+        $('body').addClass('dark-mode');
     });
 });
